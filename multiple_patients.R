141714fcc8285c1767028a34b20d7d41d51ce2d8
Nile Wilson <t-niwils@microsoft.com>
2017-10-25 10:07:24 -0700

    Changed path in multiple_patients.R and print progress per subject

--- multiple_patients.R
+++ multiple_patients.R
@@ -4,19 +4,29 @@ library(PopED)
 library(fftw)
 
 # Set your working directory here
-setwd('E:/FacesHouses')
+setwd('../FacesHouses')
 
 source('main_function_singlepatient_kai.R')
 
 # Run the main function per subject
 main_function_singlepatient_kai('de')
+print("de complete")
 main_function_singlepatient_kai('fp')
+print("fp complete")
 main_function_singlepatient_kai('ja')
+print("ja complete")
 main_function_singlepatient_kai('wc')
+print("wc complete")
 main_function_singlepatient_kai('zt')
+print("zt complete")
 main_function_singlepatient_kai('mv')
+print("mv complete")
 main_function_singlepatient_kai('aa')
+print("aa complete")
 main_function_singlepatient_kai('ap')
+print("ap complete")
 main_function_singlepatient_kai('rn')
+print("rn complete")
 main_function_singlepatient_kai('ha')
+print("ha complete")
 
